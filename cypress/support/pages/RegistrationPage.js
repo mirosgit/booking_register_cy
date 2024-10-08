cafde8393767be5389a125092eea71ae7070d357
Miros <SgtKurosaki@gmail.com>
2024-10-09 01:55:21 +0300

    Added a few negative scenarios

--- cypress/support/pages/RegistrationPage.js
+++ cypress/support/pages/RegistrationPage.js
@@ -1,15 +1,29 @@
 import { registrationPageLocators } from '../locators/registrationPageLocators.js';
+import { validationMessages } from '../config/errorMessages.js';
 
 class RegistrationPage {
-  
+
     clickRegister() {
       registrationPageLocators.elements.registrationButton().click();
     }
-  
+
     enterEmail(email) {
       registrationPageLocators.elements.emailField().type(email);
     }
-  
+
+    enterNewPassword(newPassword) {
+      registrationPageLocators.elements.newPassField().type(newPassword);
+    }
+
+    clearNewPassword() {
+      registrationPageLocators.elements.newPassField().clear();
+    } 
+
+    enterPasswords(newPassword, confirmPassword) {
+      registrationPageLocators.elements.newPassField().type(newPassword);
+      registrationPageLocators.elements.confirmPassField().type(confirmPassword);
+    }    
+
     clickContinueEmail() {
       registrationPageLocators.elements.continueEmailButton().click();
     }
@@ -17,11 +31,11 @@ class RegistrationPage {
     clickContinuePass() {
       registrationPageLocators.elements.continuePassButton().click();
     }
-    
+
     enterRandomEmailAndProceed() {
       const randomEmail = `test${Date.now()}@example.com`;
-      registrationPage.enterEmail(randomEmail);
-      registrationPage.clickContinueEmail();
+      this.enterEmail(randomEmail);
+      this.clickContinueEmail();
       cy.waitUntil(() => registrationPageLocators.elements.newPassField().should('be.visible'));
       return randomEmail;
     }
@@ -29,16 +43,33 @@ class RegistrationPage {
     checkValidationNewPassErrorVisible() {
       registrationPageLocators.elements.errorNewPassNote().should('be.visible');
     }
+
+    checkValidationConfirmPassErrorVisible() {
+      registrationPageLocators.elements.errorConfirmPassNote().should('be.visible');
+    }
+
+    checkTextConfirmPassNote(expectedText = validationMessages.PASSWORD_NOT_MATCHED) {
+      registrationPageLocators.elements.errorConfirmPassNote()
+        .should('be.visible')
+        .and('have.text', expectedText);
+    }
+
+    checkTextNewPassNote(expectedText) {
+      registrationPageLocators.elements.errorNewPassNote()
+        .should('be.visible')
+        .and('have.text', expectedText);
+    }
+
     checkValidationEmailErrorVisible() {
       registrationPageLocators.elements.errorEmailNote().should('be.visible');
     }
+
     verifyCurrentUrl(expectedUrl) {
         cy.url().then((currentUrl) => {
-        const parsedUrl = new URL(currentUrl);
-        const urlWithoutParams = `${parsedUrl.protocol}//${parsedUrl.host}${parsedUrl.pathname}`;
-        expect(urlWithoutParams).to.equal(expectedUrl);
+          const [urlWithoutParams] = currentUrl.split('?');
+          expect(urlWithoutParams).to.equal(expectedUrl);
         });
     }
-  }
-  
-  export const registrationPage = new RegistrationPage();
+}
+
+export const registrationPage = new RegistrationPage();
